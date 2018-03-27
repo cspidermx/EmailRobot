69295a7e7b8c65639f1748e1666d29ada9ccffec
cspidermx <cspidermx@yahoo.com.mx>
2018-03-27 14:17:47 -0600

    Facelift

--- emapp/__init__.py
+++ emapp/__init__.py
@@ -3,6 +3,7 @@ from config import emConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -11,8 +12,10 @@ emrdb = SQLAlchemy(app)
 migratedb = Migrate(app, emrdb)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
-from emapp import routes, models, emailfunc
+
+from emapp import routes, models, emailfunc, errors
 
 
 emailfunc.maint(1)
